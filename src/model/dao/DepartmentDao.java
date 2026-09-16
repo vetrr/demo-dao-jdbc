3c1b4ed6957597b39916d771fb4c362ae2d7b912
vettr <vitorlimamoreira01@gmail.com>
2026-09-16 18:51:42 -0300

    Criando a interface DepartmentDao.

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import model.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+    void insert(Department obj);
+    void update(Department obj);
+    void deletByID(Integer id);
+    Department findByID(Integer id);
+    List<Department> findAll();
+}
